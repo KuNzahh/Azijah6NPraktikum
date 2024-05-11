98673e4e8fbb497beebcb41f956538f04d47e2fc
noor azijah <noorazijah444@gmail.com>
2024-05-11 14:19:14 +0800

    Upload pertama apk siak

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aplikasi SIAK</title>
+</head>
+<body>
+    <h1> Selamat Datang di Aplikasi SIAK</h1>
+    Menu :
+    <ul>
+        <li><a href="/mahasiswa">MAHASISWA</a></li>
+    </ul>
+</body>
+</html>
